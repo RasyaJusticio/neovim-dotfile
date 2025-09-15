d5051afa3ad722f837d9205466ae6bd5009346a7
RasyaJusticio <rasyajusticio.public@gmail.com>
2025-09-15 16:20:35 +0700

    feat(completion): add completion plugin

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,45 +1,46 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup()
-        end
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({
-                automatic_enable = false,
-                ensure_installed = {
-                    "lua_ls",
-                    "phpactor",
-                    "laravel_ls",
-                    "emmet_ls",
-                    "eslint",
-                }
-            })
-        end
-    },
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            vim.lsp.config("lua_ls", {
-                settings = {
-                    Lua = {
-                        diagnostics = {
-                            globals = { "vim" }
-                        }
-                    }
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				automatic_enable = false,
+				ensure_installed = {
+					"lua_ls",
+					"phpactor",
+					"laravel_ls",
+					"emmet_ls",
+					"eslint",
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-                }
-            })
-            vim.lsp.enable("lua_ls")
+			vim.lsp.config("lua_ls", {
+				capabilities = capabilities,
+				settings = {
+					Lua = {
+						diagnostics = {
+							globals = { "vim" },
+						},
+					},
+				},
+			})
+			vim.lsp.enable("lua_ls")
 
-            vim.lsp.enable("phpactor")
-            vim.lsp.enable("laravel_ls")
-            vim.lsp.enable("emmet_ls")
-            vim.lsp.enable("eslint")
-        end
-    }
+			vim.lsp.enable("laravel_ls")
+			vim.lsp.enable("phpactor")
+			vim.lsp.enable("emmet_ls")
+			vim.lsp.enable("eslint")
+		end,
+	},
 }
-
