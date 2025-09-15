28da7ced7629d9b2c8ee8b257422a4e1ed64ad6d
RasyaJusticio <rasyajusticio.public@gmail.com>
2025-09-15 16:27:39 +0700

    feat(completion): switch out `nvim-cmp` into `blink.cmp`

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -23,10 +23,7 @@ return {
 	{
 		"neovim/nvim-lspconfig",
 		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
 			vim.lsp.config("lua_ls", {
-				capabilities = capabilities,
 				settings = {
 					Lua = {
 						diagnostics = {
