8a135d9d6468e1a1e9cb2a1bfbdbfeb820e3d72a
RasyaJusticio <rasyajusticio.public@gmail.com>
2025-09-15 14:39:03 +0700

    feat(options): add some options

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,7 +1,18 @@
 local opt = vim.opt
 
+-- Current line highlighting
+opt.cursorline = true
+
+-- Number options
+opt.number = true
+opt.relativenumber = true
+opt.numberwidth = 2
+
+-- Tab options
 opt.tabstop = 4
 opt.shiftwidth = 4
 opt.expandtab = true
+
+-- Terminal GUI colors
 opt.termguicolors = true
 
